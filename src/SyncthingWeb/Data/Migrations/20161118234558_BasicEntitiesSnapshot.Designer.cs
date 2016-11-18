86611aea68c8272605971f1ca50c72c2d703104c
Patryk Wąsiewicz <patryk.wasiewicz@outlook.com>
2016-11-19 00:48:26 +0100

    Migrations

--- src/SyncthingWeb/Data/Migrations/20161118234558_BasicEntitiesSnapshot.Designer.cs
+++ src/SyncthingWeb/Data/Migrations/20161118234558_BasicEntitiesSnapshot.Designer.cs
@@ -8,9 +8,10 @@ using SyncthingWeb.Data;
 namespace SyncthingWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161118234558_BasicEntitiesSnapshot")]
+    partial class BasicEntitiesSnapshot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
---
+++ src/SyncthingWeb/Data/Migrations/20161118234558_BasicEntitiesSnapshot.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SyncthingWeb.Data.Migrations
+{
+    public partial class BasicEntitiesSnapshot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Folders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FolderId = table.Column<string>(maxLength: 32767, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Folders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GeneralSettingses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AdminId = table.Column<string>(nullable: true),
+                    EnableRegistration = table.Column<bool>(nullable: false),
+                    Initialzed = table.Column<bool>(nullable: false),
+                    SyncthingConfigPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralSettingses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GeneralSettingses_AspNetUsers_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 300, nullable: true),
+                    RoleId = table.Column<string>(nullable: true),
+                    RoleId1 = table.Column<string>(nullable: true),
+                    RoleUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_AspNetUserRoles_RoleUserId_RoleId1",
+                        columns: x => new { x.RoleUserId, x.RoleId1 },
+                        principalTable: "AspNetUserRoles",
+                        principalColumns: new[] { "UserId", "RoleId" },
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AllowedFolder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FolderId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AllowedFolder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AllowedFolder_Folders_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "Folders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AllowedFolder_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SharedEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Data = table.Column<string>(nullable: true),
+                    FolderId = table.Column<int>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: false),
+                    Path = table.Column<string>(nullable: false),
+                    Provider = table.Column<Guid>(nullable: false),
+                    ShareTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SharedEntries_Folders_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "Folders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SharedEntries_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AllowedFolder_FolderId",
+                table: "AllowedFolder",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AllowedFolder_UserId",
+                table: "AllowedFolder",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneralSettingses_AdminId",
+                table: "GeneralSettingses",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_RoleUserId_RoleId1",
+                table: "RolePermissions",
+                columns: new[] { "RoleUserId", "RoleId1" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedEntries_FolderId",
+                table: "SharedEntries",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedEntries_OwnerId",
+                table: "SharedEntries",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AllowedFolder");
+
+            migrationBuilder.DropTable(
+                name: "GeneralSettingses");
+
+            migrationBuilder.DropTable(
+                name: "RolePermissions");
+
+            migrationBuilder.DropTable(
+                name: "SharedEntries");
+
+            migrationBuilder.DropTable(
+                name: "Folders");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
