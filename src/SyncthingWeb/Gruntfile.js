5ec980178b2979ad47c36581e41a6bd6dfb6cd87
Patryk Wasiewicz <patryk.wasiewicz@outlook.com>
2016-12-02 12:26:25 +0100

    Bower components moved to other folder, grunt is used to build copy files to wwwroot.

---
+++ src/SyncthingWeb/Gruntfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding AfterBuild='build' Clean='clean' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ["wwwroot/lib/*", "temp/"],
+        copy: {
+            main: {
+                files: [
+                  { expand: true, cwd: "bower_components/bootstrap/dist", src: ['**'], dest: 'wwwroot/lib/bootstrap/dist/' },
+                  { expand: true, cwd: "bower_components/jquery/dist", src: ['**'], dest: 'wwwroot/lib/jquery/dist/' },
+                  { expand: true, cwd: "bower_components/jquery-validation/dist", src: ['**'], dest: 'wwwroot/lib/jquery-validation/dist/' },
+                  { expand: true, cwd: "bower_components/jquery-validation-unobtrusive/", src: ['*.js'], dest: 'wwwroot/lib/jquery-validation-unobtrusive/' },
+                  { expand: true, cwd: "bower_components/select2/dist", src: ['**'], dest: 'wwwroot/lib/select2/dist/' }
+                ],
+            },
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask("build", ['copy']);
+};
