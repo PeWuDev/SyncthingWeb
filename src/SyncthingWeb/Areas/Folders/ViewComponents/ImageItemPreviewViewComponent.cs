208d390a281ea4dcff397bc25bdbd29f478c18ee
Patryk Wąsiewicz <patryk.wasiewicz@outlook.com>
2018-11-18 16:11:35 +0100

    Updated packages & chnaged resize alg

--- src/SyncthingWeb/Areas/Folders/ViewComponents/ImageItemPreviewViewComponent.cs
+++ src/SyncthingWeb/Areas/Folders/ViewComponents/ImageItemPreviewViewComponent.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using ImageSharp;
-using ImageSharp.Formats;
-using ImageSharp.Processing;
-using Microsoft.AspNetCore.Mvc;
-using SixLabors.Primitives;
+﻿using Microsoft.AspNetCore.Mvc;
 using SyncthingWeb.Areas.Folders.Models;
 using SyncthingWeb.Areas.Folders.Services;
 using SyncthingWeb.Caching;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SyncthingWeb.Areas.Folders.ViewComponents
 {
@@ -20,7 +20,7 @@ namespace SyncthingWeb.Areas.Folders.ViewComponents
         public ImageItemPreviewViewComponent(ISyncthingFileFetcher fileFetcher, ICache cache)
         {
             _fileFetcher = fileFetcher;
-            this._cache = cache;
+            _cache = cache;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(PreviewableFileEntryContext context)
@@ -33,7 +33,7 @@ namespace SyncthingWeb.Areas.Folders.ViewComponents
         private async  Task<string> PreviewImageDataForAsync(FileEntryContext context)
         {
             var cacheKey = $"{context.FolderId}-{context.Path}-{context.Name}";
-            return await this._cache.GetAsync(cacheKey, async cacheContext =>
+            return await _cache.GetAsync(cacheKey, async cacheContext =>
             {
                 var path =
                     (await _fileFetcher.GetFileToDownloadAsync(context.FolderId,
@@ -59,14 +59,36 @@ namespace SyncthingWeb.Areas.Folders.ViewComponents
         {
             using (var ms = new MemoryStream())
             {
-                using (var image = Image.Load(Configuration.Default, file))
+                using (var image = new Bitmap(Image.FromStream(file)))
                 {
-                    image.Resize(new ResizeOptions
+                    const int size = 90;
+                    int width, height;
+                    if (image.Width > image.Height)
                     {
-                        Size = new Size(90, 90),
-                        Mode = ResizeMode.Crop,
-                        Position = AnchorPosition.Center
-                    }).Save(ms, new JpegEncoder());
+                        width = size;
+                        height = Convert.ToInt32(image.Height * size / (double)image.Width);
+                    }
+                    else
+                    {
+                        width = Convert.ToInt32(image.Width * size / (double)image.Height);
+                        height = size;
+                    }
+
+                    var resized = new Bitmap(width, height);
+                    using (var graphics = Graphics.FromImage(resized))
+                    {
+                        graphics.CompositingQuality = CompositingQuality.HighSpeed;
+                        graphics.InterpolationMode = InterpolationMode.Bilinear;
+                        graphics.CompositingMode = CompositingMode.SourceCopy;
+                        graphics.DrawImage(image, 0, 0, 90, 90);
+
+                        var qualityParamId = Encoder.Quality;
+                        var encoderParameters =
+                            new EncoderParameters(1) {Param = {[0] = new EncoderParameter(qualityParamId, 75)}};
+                        var codec = ImageCodecInfo.GetImageDecoders()
+                            .FirstOrDefault(cdc => cdc.FormatID == ImageFormat.Jpeg.Guid);
+                        resized.Save(ms, codec, encoderParameters);
+                    }
                 }
 
                 ms.Position = 0;
